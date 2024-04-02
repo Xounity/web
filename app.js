7385b08d75607f18e56aa204ab8a167146e3dc38
Dantesos33 <dantesos33@gmail.com>
2024-04-02 13:45:58 +0500

    goto top button added, mobile nav still not fixed

--- app.js
+++ app.js
@@ -72,7 +72,26 @@ let mob = document.querySelectorAll('.mob');
     el.addEventListener('click',()=>{
       console.log(el);
       mobileMenu.style.left = '-100%';
+      document.body.classList.remove('menu-open');
       menuIcon.classList.remove('fa-xmark');
       menuIcon.classList.add('fa-bars');
     });
   });
+
+  // Goto button
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
