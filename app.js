d124155a4ed8325837757bebb3e7a94d89b52ffa
Dantesos33 <dantesos33@gmail.com>
2024-04-02 14:36:04 +0500

    about fixed and navbar fixed

--- app.js
+++ app.js
@@ -1,49 +1,56 @@
-let mobileMenu = document.querySelector('.mobile-menu');
-    let menuIcon = document.querySelector('.hamburger');
-    menuIcon.addEventListener('click',()=>{
-      if(mobileMenu.style.left !== '0px'){
-        mobileMenu.style.left = '0px';
-      menuIcon.classList.remove('fa-bars');
-      menuIcon.classList.add('fa-xmark');
-      document.body.style.overflow = 'hidden';
-      }
-      else{
-        mobileMenu.style.left = '-100%';
-      menuIcon.classList.remove('fa-xmark');
-      menuIcon.classList.add('fa-bars');
-      document.body.style.overflow = 'auto';
-      }
-    });
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      const target = this.getAttribute('href');
-      const targetElement = document.querySelector(target);
-      const topOffset = targetElement.getBoundingClientRect().top;
-      window.scrollTo({
-        top: topOffset,
-        behavior: 'smooth'
-      });
-    });
+window.addEventListener('load', () => {
+  console.clear();
+})
 
-// navbar hide/unhide on scroll
-    let prevScrollPos = window.pageYOffset;
-    let navBar = document.querySelector('header');
-    window.onscroll = function () {
-      let currentScrollPos = window.pageYOffset;
-      if (prevScrollPos > currentScrollPos) {
-        navBar.style.transform = 'translateY(0)';
-        navBar.style.transition = 'transform 0.5s';
-      } else {
-        navBar.style.transform = 'translateY(-100%)';
-        navBar.style.transition = 'transform 0.5s';
-      }
-      prevScrollPos = currentScrollPos;
-      
-    }
+let mobileMenu = document.querySelector('.mobile-menu');
+let menuIcon = document.querySelector('.hamburger');
+menuIcon.addEventListener('click', () => {
+  if (mobileMenu.style.left !== '0px') {
+    mobileMenu.style.left = '0px';
+    menuIcon.classList.remove('fa-bars');
+    menuIcon.classList.add('fa-xmark');
+    document.body.style.overflow = 'hidden';
+  }
+  else {
+    mobileMenu.style.left = '-100%';
+    menuIcon.classList.remove('fa-xmark');
+    menuIcon.classList.add('fa-bars');
+    document.body.style.overflow = 'auto';
+  }
+});
+
+// Scroll Animation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = this.getAttribute('href');
+    const targetElement = document.querySelector(target);
+    const topOffset = targetElement.getBoundingClientRect().top;
+    window.scrollTo({
+      top: topOffset,
+      behavior: 'smooth'
+    });
   });
 
-const iconsArray = Array.from({length: 8}, (_, i) => document.querySelector(`#s${i+1}`));
+  // navbar hide/unhide on scroll
+  // let prevScrollPos = window.pageYOffset;
+  // let navBar = document.querySelector('header');
+  // window.onscroll = function () {
+  //   let currentScrollPos = window.pageYOffset;
+  //   if (prevScrollPos > currentScrollPos) {
+  //     navBar.style.transform = 'translateY(0)';
+  //     navBar.style.transition = 'transform 0.5s';
+  //   } else {
+  //     navBar.style.transform = 'translateY(-100%)';
+  //     navBar.style.transition = 'transform 0.5s';
+  //   }
+  //   prevScrollPos = currentScrollPos;
+
+  // }
+});
+
+// Icons animation
+const iconsArray = Array.from({ length: 8 }, (_, i) => document.querySelector(`#s${i + 1}`));
 
 function handleMouseOver(opac, scale) {
   this.style.transform = `scale(${scale})`;
@@ -54,8 +61,8 @@ function handleMouseOver(opac, scale) {
     }
   });
 }
-iconsArray.forEach(function(icon) {
-  icon.addEventListener('mouseover', function() {
+iconsArray.forEach(function (icon) {
+  icon.addEventListener('mouseover', function () {
     handleMouseOver.call(this, 0.50, 1.3);
   });
   icon.addEventListener('mouseout', handleMouseOut);
@@ -68,20 +75,20 @@ function handleMouseOut() {
 // Mobile Nav Links
 let mob = document.querySelectorAll('.mob');
 
-  mob.forEach(el => {
-    el.addEventListener('click',()=>{
-      console.log(el);
-      mobileMenu.style.left = '-100%';
-      document.body.classList.remove('menu-open');
-      menuIcon.classList.remove('fa-xmark');
-      menuIcon.classList.add('fa-bars');
-    });
+mob.forEach(el => {
+  el.addEventListener('click', () => {
+    console.log(el);
+    mobileMenu.style.left = '-100%';
+    document.body.style.overflow = 'auto';
+    menuIcon.classList.remove('fa-xmark');
+    menuIcon.classList.add('fa-bars');
   });
+});
 
-  // Goto button
+// Goto button
 let mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -92,6 +99,6 @@ function scrollFunction() {
 }
 
 function topFunction() {
-  document.body.scrollTop = 0; 
-  document.documentElement.scrollTop = 0; 
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
